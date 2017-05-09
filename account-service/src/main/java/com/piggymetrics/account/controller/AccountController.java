71f7cf387f314587a602cc930dcd19a1ef41ed67
Adam Dyrek <adam.dyrek@codeconcept.pl>
2017-05-09 17:46:49 +0200

    - fix accountcontroller

--- account-service/src/main/java/com/piggymetrics/account/controller/AccountController.java
+++ account-service/src/main/java/com/piggymetrics/account/controller/AccountController.java
@@ -36,19 +36,4 @@ public class AccountController {
 	public Account createNewAccount(@Valid @RequestBody User user) {
 		return accountService.create(user);
 	}
-
-	@RequestMapping(path = "/", method = RequestMethod.POST)
-	public Account createNewAccount2(@Valid @RequestBody User user) {
-		return accountService.create(user);
-	}
-
-	@RequestMapping(path = "/", method = RequestMethod.POST)
-	public Account createNewAccount3(@Valid @RequestBody User user) {
-		return accountService.create(user);
-	}
-
-    @RequestMapping(path = "/", method = RequestMethod.POST)
-    public Account createNewAccount4(@Valid @RequestBody User user) {
-        return accountService.create(user);
-    }
 }
